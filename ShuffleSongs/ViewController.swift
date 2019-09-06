3216133bcf4f59a96e54b36eb02c71a92f6f7efc
Paulo Henrique dos Santos <paulo.santos@poatek.com>
2019-09-05 21:38:06 -0300

    Initial Commit

---
+++ ShuffleSongs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShuffleSongs
+//
+//  Created by Paulo Henrique dos Santos on 05/09/19.
+//  Copyright © 2019 Paulo Henrique dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
